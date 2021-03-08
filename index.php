e4a4ca33a815cb90829b014564e866e8a0a6221f
Karl Murray <karlm89@me.com>
2021-03-08 06:09:01 -0600

    first commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Learning PHP</title>
+</head>
+<body>
+    <h1>Challenges</h1>
+    <h2>Section 3</h2>
+        <ol>
+            <li><a href="/section-3/1-backToSchool.php">Back To School</a></li>
+        </ol>
+    <h2>Section 4</h2>
+        <ol>
+            <li><a href="/section-4/2-whatsInAName.php">What's in a Name?</a></li>
+        </ol>
+    <h2>Section 6</h2>
+        <ol>
+            <li><a href="/section-6/3-multiples.php">Multiples</a></li>
+            <li><a href="/section-6/4-fizzBuzz.php">Fizz Buzz</a></li>
+        </ol>
+</body>
+</html>
---
+++ section-3/1-backToSchool.php
@@ -0,0 +1,9 @@
+<?php
+
+$base = 6;
+$height = 4;
+$r = 10;
+
+echo 'Area of a triangle: '. ($base * $height) / 2 . '<br><br>';
+
+echo 'Area of a circle: '. 3.14*pow($r, 2);
